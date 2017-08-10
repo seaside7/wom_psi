5af9a02bf97c1facef651beafb988c0288d7e290
carlitomorales <carlito_morales@yahoo.co.id>
2017-08-10 20:11:36 +0700

    push test

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ require_once('function/mainfunction.php');
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<title>WOM Finance | Psikotest Online</title>
+	<title>WOM Finance | Psikotest Online!</title>
 	<link href="css/custom2.css" rel="stylesheet" type="text/css">
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
